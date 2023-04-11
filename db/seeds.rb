3dca8846869d303aae3c83d3eeb485c602f0203d
dorian fiolet <dorian3012@hotmail.fr>
2023-04-11 12:09:48 +0200

    save before sqale

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,17 +21,17 @@ user2 = User.create(email: "dorian2@gmail.com", password: "coucou")
 
 
 file = URI.open("https://lh3.googleusercontent.com/d9iOns4Q6RQl4ozES9D1hsaSfTad0b1LLYX64K1u9x9PuFcMa61uhw6K_IS8ukm6yKSZJOfC-nhnPuEAFCSmuRZQISzV3a1gm843UZWTrs6huQ=s750")
-vehicule1 = Vehicule.create(marque: "ALFA ROMEO", modele: "350z", user_id: user1.id, enregistrement: "2017", kilometrage: 37500, taille: 2.0, puissancech: 120, puissancecv: 5, carburant: "Diesel", boite: "Automatique", prix: 23000, carrosserie: "Break", couleur: "Rouge", proprietaires: 2, dioxy: 5, portes: 5, places: 5)
+vehicule1 = Vehicule.create(marque: "ALFA ROMEO", modele: "350z", user_id: user1.id, enregistrement: "2017", kilometrage: 37500, taille: 2.3, puissancech: 120, puissancecv: 5, carburant: "Diesel", boite: "Automatique", prix: 23006.67, carrosserie: "Break", couleur: "Rouge", proprietaires: 2, dioxy: 5, portes: 5, places: 5)
 vehicule1.photo.attach(io: file, filename: "v1.png", content_type: "image/png")
 vehicule1.save
 
 file = URI.open("https://www.challenges.fr/assets/img/2017/11/02/cover-r4x3w1200-59fb0424953e9-jeep-wrangler-unlimited-rubicon-24-jpeg.jpg")
-vehicule2 = Vehicule.create(marque: "JEEP", modele: "350z", user_id: user1.id, enregistrement: 2010, kilometrage: 110000, taille: 1.9, puissancech: 180, puissancecv: 7, carburant: "Diesel", boite: "Manuelle", prix: 27000, carrosserie: "Cross", couleur: "Rouge", proprietaires: 2, dioxy: 5, portes: 5, places: 5)
+vehicule2 = Vehicule.create(marque: "JEEP", modele: "350z", user_id: user1.id, enregistrement: 2010, kilometrage: 110000, taille: 1.9, puissancech: 180, puissancecv: 7, carburant: "Diesel", boite: "Manuelle", prix: 27000.54, carrosserie: "Cross", couleur: "Rouge", proprietaires: 2, dioxy: 5, portes: 5, places: 5)
 vehicule2.photo.attach(io: file, filename: "v2.png", content_type: "image/png")
 vehicule2.save
 
 file = Rails.root.join('app', 'assets', 'images', 'car1.jpg')
-vehicule3 = Vehicule.create(marque: "ALFA ROMEO", modele: "Giulia", user_id: user1.id, enregistrement: 2017, kilometrage: 53200, taille: 2.0, puissancech: 280, puissancecv: 5, carburant: "Essence", boite: "Manuelle", prix: 36990, carrosserie: "Sport", couleur: "Rouge", proprietaires: 2, dioxy: 5, portes: 5, places: 5)
+vehicule3 = Vehicule.create(marque: "ALFA ROMEO", modele: "Giulia", user_id: user1.id, enregistrement: 2017, kilometrage: 53200, taille: 2.0, puissancech: 280, puissancecv: 5, carburant: "Essence", boite: "Manuelle", prix: 36990.45, carrosserie: "Sport", couleur: "Rouge", proprietaires: 2, dioxy: 5, portes: 5, places: 5)
 vehicule3.photo.attach(io: File.open(file), filename: "v3.jpg", content_type: "image/jpeg")
 vehicule3.save
 
