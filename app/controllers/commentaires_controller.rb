cbee28b361bbacb76e5ee5b97df5542a24395236
dorian fiolet <dorian3012@hotmail.fr>
2023-04-15 17:01:09 +0200

    save comment add before validate

---
+++ app/controllers/commentaires_controller.rb
@@ -0,0 +1,43 @@
+class CommentairesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: %i[index new create]
+
+  def index
+    @commentaires = Commentaire.all
+    @commentaire = Commentaire.new
+  end
+
+
+  def create
+    @commentaire = Commentaire.new(commentaire_params)
+    if @commentaire.save
+      redirect_to commentaires_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @commentaire = Commentaire.find(params[:id])
+  end
+
+  def update
+    @commentaire = Commentaire.find(params[:id])
+    @commentaire.update(commentaire_params)
+    @commentaire.save
+    redirect_to commentaire_path(@commentaire)
+  end
+
+  def destroy
+    @commentaire = Commentaire.find(params[:id])
+    @commentaire.destroy
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  private
+
+  def commentaire_params
+    params.require(:commentaire).permit(:nom, :prenom, :email, :text)
+  end
+
+end
