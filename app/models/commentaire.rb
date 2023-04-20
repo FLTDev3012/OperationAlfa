4f775fe0894b99afe677584fba7d2fd8c7736091
dorian fiolet <dorian3012@hotmail.fr>
2023-04-20 13:00:19 +0200

    regex email

--- app/models/commentaire.rb
+++ app/models/commentaire.rb
@@ -1,5 +1,6 @@
 class Commentaire < ApplicationRecord
   validates :nom, presence: true, length: { minimum: 3, maximum: 20, message: "( 3 caractères minimum )" }
   validates :prenom, presence: true, length: { minimum: 3, maximum: 20, message: "( 3 caractères minimum )" }
+  validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/ }
   validates :text, presence: true, length: { minimum: 10, maximum: 2000, message: "( 10 caractères minimum )" }
 end
