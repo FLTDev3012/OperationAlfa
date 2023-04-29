491c4a36cd199122bb05c372e8c10633088f29c6
dorian fiolet <dorian3012@hotmail.fr>
2023-04-29 10:53:00 +0200

    savejsfini

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -2,15 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dashboard"
 export default class extends Controller {
-  static targets = ["premierepage", "deuxiemepage", "troisiemepage" ]
+  static targets = ["sectionvehicule", "sectionpiece", "sectioncom", "sectionmember" ]
   connect() {
     console.log("Dashboard connected")
   }
 
-  update1() {
+  openvehicule() {
     // au clic on passe en display
-    this.premierepageTarget.classList.remove("d-none")
-    this.deuxiemepageTarget.classList.add("d-none")
-    this.troisiemepageTarget.classList.add("d-none")
+    this.sectionvehiculeTarget.classList.remove("d-none")
+    this.sectionpieceTarget.classList.add("d-none")
+    this.sectioncomTarget.classList.add("d-none")
+    this.sectionmemberTarget.classList.add("d-none")
+  }
+
+  openpiece() {
+    // au clic on passe en display
+    this.sectionvehiculeTarget.classList.add("d-none")
+    this.sectionpieceTarget.classList.remove("d-none")
+    this.sectioncomTarget.classList.add("d-none")
+    this.sectionmemberTarget.classList.add("d-none")
+  }
+
+  opencom() {
+    // au clic on passe en display
+    this.sectionvehiculeTarget.classList.add("d-none")
+    this.sectionpieceTarget.classList.add("d-none")
+    this.sectioncomTarget.classList.remove("d-none")
+    this.sectionmemberTarget.classList.add("d-none")
+  }
+
+  openmember() {
+    // au clic on passe en display
+    this.sectionvehiculeTarget.classList.add("d-none")
+    this.sectionpieceTarget.classList.add("d-none")
+    this.sectioncomTarget.classList.add("d-none")
+    this.sectionmemberTarget.classList.remove("d-none")
   }
 }
