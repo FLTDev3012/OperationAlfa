1c6f61b63e3218faeb6dd9d5eca32cc7f8ee9561
dorian fiolet <dorian3012@hotmail.fr>
2023-04-30 12:43:30 +0200

    save

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -2,6 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="filter"
 export default class extends Controller {
+  static targets = ["sectionBtnFilter", "sectionBigFilter" ]
   connect() {
+    console.log("Filter js connected")
   }
+
+  openBigFilter() {
+    // au clic on passe en display
+    this.sectionBigFilterTarget.classList.remove("d-none")
+    this.sectionBtnFilterTarget.classList.add("d-none")
+  }
+
+
 }
