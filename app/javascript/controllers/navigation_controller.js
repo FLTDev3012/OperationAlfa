5a3b49c34f7a2a0e52195a6e20f5a69097c2a220
dorian fiolet <dorian3012@hotmail.fr>
2023-05-09 17:07:09 +0200

    save

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -2,17 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navigation"
 export default class extends Controller {
-  static targets = ["popnav"]
+  static targets = ["popnav", "lignes", "croix"]
   connect() {
     console.log("nav js connected")
   }
 
   openNav() {
     this.popnavTarget.classList.remove("d-none")
+    this.lignesTarget.classList.add("d-none")
+    this.croixTarget.classList.remove("d-none")
+
   }
 
 
   closeNav() {
-    this.popnavTarget.classList.remove("d-none")
+    this.popnavTarget.classList.add("d-none")
+    this.croixTarget.classList.add("d-none")
+    this.lignesTarget.classList.remove("d-none")
   }
 }
