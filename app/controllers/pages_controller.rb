b5c2b54bd8e6e1102de46334c2599e70f63d8a13
dorian fiolet <dorian3012@hotmail.fr>
2023-05-11 15:49:59 +0200

    la mapfini

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :services ]
+  skip_before_action :authenticate_user!, only: [ :home, :services, :about ]
 
   def home
     @vehicules = Vehicule.order(:created_at).last(5)
     @vehicule1 = Vehicule.order(:created_at).last
     @vehicule2 = Vehicule.order(:created_at).second_to_last
     @vehicule3 = Vehicule.order(:created_at).third_to_last
-
-
-
   end
 
   def dashboard
@@ -19,6 +16,19 @@ class PagesController < ApplicationController
   end
 
   def services
+  end
+
+  def about
+    @flats = Flat.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        info_window_html: render_to_string(partial: "flats/info_window", locals: {flat: flat}),
+        marker_html: render_to_string(partial: "flats/marker")
+      }
+    end
   end
 
 
