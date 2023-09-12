761060529465497ff68854e23de8fdb8698fd9cc
dorian fiolet <dorian3012@hotmail.fr>
2023-09-12 10:51:03 +0200

    good car

--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -7,16 +7,11 @@ class Vehicule < ApplicationRecord
   validates :marque, presence: true
   validates :modele, presence: true
   validates :enregistrement, presence: true
-  validates :taille, presence: true
   validates :puissancech, presence: true
   validates :puissancecv, presence: true
   validates :carburant, presence: true
   validates :boite, presence: true
   validates :carrosserie, presence: true
   validates :couleur, presence: true
-  validates :proprietaires, presence: true
-  validates :dioxy, presence: true
-  validates :portes, presence: true
-  validates :places, presence: true
   validates :photo, presence: true
 end
